579cabe5817dfdf8238b62e59a564ddc6fcc8dd8
Shivanshu <shivanshu.coll@gmail.com>
2026-01-06 14:10:12 +0530

    created few route handlers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,20 @@ const express = require('express');
 
 const app = express();
 
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Shiva", lastName: "Sahu"});
+});
+
+app.post("/user", (req, res) => {
+    // Logic to create a new user would go here
+    res.send("User added successfully");
+});
+
+app.delete("/user", (req, res) => {
+    // Logic to delete a user would go here
+    res.send("User deleted successfully");
+})
+
 app.use("/", (req, res) => {
     res.send("Hello babe");
 });
