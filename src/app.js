a6d649a2b7a6efc602940c7db827709fe8fe53a8
Shivanshu <shivanshu.coll@gmail.com>
2026-01-06 16:50:16 +0530

    handling multiple reqs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 const express = require('express');
-
 const app = express();
-app.use(express.json());
+
+app.use("/user", 
+    [(req, res, next) => {
+        console.log("Handling the route user");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling the route user 2");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling the route user 3");
+        res.send("3rd response");
+    }]
+);
 
 app.get("/user/:userId/:name", (req, res) => {
     console.log({...req.query});
@@ -18,8 +31,6 @@ app.delete("/user", (req, res) => {
     // Logic to delete a user would go here
     res.send("User deleted successfully");
 })
-
-
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
